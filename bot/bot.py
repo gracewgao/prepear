21c9216b7ddd8ca869043db70f366473f70664c1
Bonnie Chin <bonniechin.mail@gmail.com>
2021-01-16 17:05:36 -0500

    oh no

--- bot/bot.py
+++ bot/bot.py
@@ -9,6 +9,8 @@ import random
 
 client = commands.Bot(command_prefix = '!')
 
+
+
 @client.event
 async def on_ready():
     print('Logged in as: {0} - {1}'.format(client.user.name, client.user.id))
